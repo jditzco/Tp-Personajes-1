83adc4812af2bc1abbf3401d320ec5465e22f682
Jeronimo Daniel Itzcovich <jditzco@hotmail.com>
2023-06-28 10:12:02 -0300

    asdasd

--- src/modules/log-helper.js
+++
@@ -1,8 +0,0 @@
-import logConfiguration from "./log-configuration.js";
-import winston from "winston";
-
-
-const logger = winston.createLogger(logConfiguration);
-
-module.exports = logger;
-//logger.error("Hello, Winston logger, the first error!");
